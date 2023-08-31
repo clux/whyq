36df4ddb0fa1f6d9438b5ca9f97c3b7edbb0c4f3
clux <sszynrae@gmail.com>
2023-08-31 09:42:39 +0100

    downcast from yaml to json using singleton_map_recursive
    Signed-off-by: clux <sszynrae@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
-use serde_yaml;
-use serde_json;
-use std::io::{self, BufRead};
-use anyhow::{Result, Error};
+use anyhow::{Error, Result};
+use serde_json::{self, Value};
+use serde_yaml::{self, with::singleton_map_recursive as smr};
 
 fn main() -> Result<()> {
-    let stdin = io::stdin();
+    let stdin = std::io::stdin();
     let yaml_de = serde_yaml::Deserializer::from_reader(stdin);
-    let value = serde_yaml::Value::deserialize(yaml_de)?;
-    let json = serde_json::from_reader(value)?;
-    let ser = serde_json::to_string(json)?;
+    let mut docs: Vec<Value> = vec![];
+    for doc in yaml_de {
+        docs.push(smr::deserialize(doc)?);
+    }
+    let ser = serde_json::to_string(&docs)?;
     println!("{}", ser);
     Ok(())
 }
