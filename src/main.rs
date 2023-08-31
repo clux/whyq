8984998f8d48e4630f5d9ce8197a83610f400d2f
clux <sszynrae@gmail.com>
2023-08-31 21:37:50 +0100

    factor out file reading
    Signed-off-by: clux <sszynrae@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -9,35 +9,33 @@ use tracing::*;
 #[tokio::main]
 async fn main() -> Result<()> {
     tracing_subscriber::fmt().with_max_level(Level::INFO).init();
-    let stdin = std::io::stdin();
-    // TODO: read from file arg if no stdin
-    let yaml_de = serde_yaml::Deserializer::from_reader(stdin);
-    let mut docs: Vec<Value> = vec![];
-    for doc in yaml_de {
-        docs.push(smr::deserialize(doc)?);
-    }
-    let ser = serde_json::to_vec(&docs)?;
-    debug!("decoded json: {}", String::from_utf8_lossy(&ser));
     // pass on args, skip arg 0 (which is yq)
-    let mut all_args = std::env::args().into_iter().skip(1).collect::<Vec<_>>();
+    let all_args = std::env::args().into_iter().skip(1).collect::<Vec<_>>();
     let yaml_output = all_args.contains(&"-y".to_string());
     let args = all_args
         .into_iter()
-        .filter(|x| x != "-y")
+        .filter(|x| x != "-y") // yq only arg
         .collect::<Vec<_>>();
-    // TODO: slice off -y arg
     debug!("args: {:?}", args);
+
+    // read file input either from file or stdin
+    let input = read_input_yaml(&args.last()).await?;
+
+    // shellout jq with given args
     let mut child = Command::new("jq")
         .args(args)
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
         .stderr(Stdio::inherit())
         .spawn()?;
+    // pass file input as stdin
     let mut stdin = child.stdin.take().unwrap();
-    stdin.write_all(&ser).await.unwrap();
+    stdin.write_all(&input).await.unwrap();
     drop(stdin);
+    // then wait for exit and gather output
     let output = child.wait_with_output().await?;
     let stdout = output.stdout;
+    // print output either as yaml or json (as per jq output)
     if yaml_output {
         let val: serde_json::Value = serde_json::from_slice(&stdout)?;
         let ser2 = serde_yaml::to_string(&val)?;
@@ -46,4 +44,29 @@ async fn main() -> Result<()> {
         println!("{}", String::from_utf8_lossy(&stdout));
     }
     Ok(())
+}
+
+/// Convert yaml input into vector of json encoded bytes
+async fn read_input_yaml(last_arg: &Option<&String>) -> Result<Vec<u8>> {
+    let contents; // long lived scope for file case
+    let yaml_de = if let Some(last) = last_arg {
+        if let Ok(true) = tokio::fs::try_exists(last).await {
+            contents = tokio::fs::read_to_string(last).await?;
+            serde_yaml::Deserializer::from_str(&contents)
+        } else {
+            let stdin = std::io::stdin();
+            serde_yaml::Deserializer::from_reader(stdin)
+        }
+    } else {
+        let stdin = std::io::stdin();
+        serde_yaml::Deserializer::from_reader(stdin)
+    };
+
+    let mut docs: Vec<Value> = vec![];
+    for doc in yaml_de {
+        docs.push(smr::deserialize(doc)?);
+    }
+    let ser = serde_json::to_vec(&docs)?;
+    debug!("decoded json: {}", String::from_utf8_lossy(&ser));
+    Ok(ser)
 }
