a8ad2b78b9d84e48878e54afb3c970b324298b34
clux <sszynrae@gmail.com>
2023-08-31 20:58:29 +0100

    basic functionality working
    Signed-off-by: clux <sszynrae@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use anyhow::{bail, Error, Result};
+use anyhow::Result;
 use serde_json::{self, Value};
 use serde_yaml::{self, with::singleton_map_recursive as smr};
 use std::process::Stdio;
 use tokio::io::AsyncWriteExt;
-use tokio::process::{ChildStdin, Command};
+use tokio::process::Command;
 use tracing::*;
 
 #[tokio::main]
@@ -19,31 +19,31 @@ async fn main() -> Result<()> {
     let ser = serde_json::to_vec(&docs)?;
     debug!("decoded json: {}", String::from_utf8_lossy(&ser));
     // pass on args, skip arg 0 (which is yq)
-    let args = std::env::args().into_iter().skip(1).collect::<Vec<_>>();
+    let mut all_args = std::env::args().into_iter().skip(1).collect::<Vec<_>>();
+    let yaml_output = all_args.contains(&"-y".to_string());
+    let args = all_args
+        .into_iter()
+        .filter(|x| x != "-y")
+        .collect::<Vec<_>>();
     // TODO: slice off -y arg
     debug!("args: {:?}", args);
-    let mut cmd = Command::new("jq")
+    let mut child = Command::new("jq")
         .args(args)
         .stdin(Stdio::piped())
-        .stdout(Stdio::inherit())
+        .stdout(Stdio::piped())
         .stderr(Stdio::inherit())
         .spawn()?;
-    let mut stdin = cmd.stdin.take().expect("Failed to open stdin");
-    stdin
-        .write_all(&ser)
-        .await
-        .expect("Failed to write to stdin");
-    stdin.write(b"\n").await?;
-    stdin.flush().await?;
-    //stdin.poll_shutdown().await?;
-    //let output = cmd.wait_with_output().await.unwrap();
-    //info!("done, {:?}", output);
-    //if output.stderr.is_empty() {
-    //    info!("ww {}", String::from_utf8_lossy(&output.stdout));
-    //    Ok(())
-    //} else {
-    //    warn!("{:?}", String::from_utf8_lossy(&output.stderr));
-    //    Ok(())
-    //}
+    let mut stdin = child.stdin.take().unwrap();
+    stdin.write_all(&ser).await.unwrap();
+    drop(stdin);
+    let output = child.wait_with_output().await?;
+    let stdout = output.stdout;
+    if yaml_output {
+        let val: serde_json::Value = serde_json::from_slice(&stdout)?;
+        let ser2 = serde_yaml::to_string(&val)?;
+        println!("{}", ser2);
+    } else {
+        println!("{}", String::from_utf8_lossy(&stdout));
+    }
     Ok(())
 }
